c8e75c04d4a6fda595f193a1ccf3c152c7f42e0c
jneo8 <jneo8lin@gmail.com>
2019-01-30 18:20:31 +0800

    pipfile

---
+++ main.py
@@ -0,0 +1,15 @@
+from pyspark import SparkConf
+from pyspark import SparkContext
+import numpy as np
+
+conf = SparkConf()
+conf.setMaster('spark://localhost:7077')
+conf.setAppName('spark-basic')
+print(dir(conf))
+sc = SparkContext(conf=conf)
+
+def mod(x):
+    return (x, np.mod(x, 2))
+
+rdd = sc.parallelize(range(1000)).map(mod).take(10)
+print(rdd)
