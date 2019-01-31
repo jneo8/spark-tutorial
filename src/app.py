0dbcc3ce2934cb3f278f03019fc21b34693d000c
jneo8 <jneo8lin@gmail.com>
2019-01-31 16:30:01 +0800

    src

---
+++ src/app.py
@@ -0,0 +1,14 @@
+from pyspark import SparkConf
+from pyspark import SparkContext
+
+conf = SparkConf()
+conf.setMaster('spark://spark-master:7077')
+conf.setAppName('spark-basic')
+print(dir(conf))
+sc = SparkContext(conf=conf)
+
+def mod(x):
+    return (x, x % 2)
+
+rdd = sc.parallelize(range(1000)).map(mod).take(10)
+print(rdd)
